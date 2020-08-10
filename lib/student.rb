3ce6c9c4b8ad94e55c0fcbb55b69ef0f74db848a
Marcus Moreno <legendary.1198@gmail.com>
2020-08-10 16:45:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,11 +4,12 @@ attr_accessor :name, :grade
 
 attr_reader :id 
 
-def initialize(name, grade, id=nil)
+  def initialize(name, grade, id=nil)
   
-  @id = id 
-  @name = name
-  @grade = grade
+    @id = id 
+    @name = name
+    @grade = grade
+
 end
 
 end
